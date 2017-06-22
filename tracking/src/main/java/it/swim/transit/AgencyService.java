a606070759501e7e15f8de4b40a3d8d0c7b3114f
Prithvi Gudapati <prithvi.gudapati98@gmail.com>
2017-06-21 17:00:20 -0700

    Formatting

--- tracking/src/main/java/it/swim/transit/AgencyService.java
+++ tracking/src/main/java/it/swim/transit/AgencyService.java
@@ -11,43 +11,43 @@ import it.swim.recon.Value;;
 
 public class AgencyService extends AbstractService {
 
-	private String agency = "";
+    private String agency = "";
 
-	@SwimLane("agency/vehicles")
-	public MapLane<String, Value> vehiclesMap = mapLane().keyClass(String.class).valueClass(Value.class);
+    @SwimLane("agency/vehicles")
+    public MapLane<String, Value> vehiclesMap = mapLane().keyClass(String.class).valueClass(Value.class);
 
     @SwimLane("agency/count")
-	public ValueLane<Integer> vehiclesCount = valueLane().valueClass(Integer.class);
+    public ValueLane<Integer> vehiclesCount = valueLane().valueClass(Integer.class);
 
-	@SwimLane("agency/speed")
+    @SwimLane("agency/speed")
     public ValueLane<Float> vehiclesSpeed = valueLane().valueClass(Float.class);
 
-	@SwimLane("agency/set")
-	public CommandLane<String> agencySet = commandLane().valueClass(String.class).onCommand((String info) -> agency = info);
+    @SwimLane("agency/set")
+    public CommandLane<String> agencySet = commandLane().valueClass(String.class).onCommand((String info) -> agency = info);
 
-	private void checkVehicleLocations() {
-		Value[] vehicles = NextBusHttpAPI.getVehicleLocations(agency);
-		vehiclesMap.clear();
-		int speedSum = 0;
-		for (int i = 0; i < vehicles.length; i++) {
-			vehiclesMap.put(vehicles[i].get("id").stringValue(), vehicles[i]);
-			speedSum+= Integer.parseInt(vehicles[i].get("speed").stringValue());
-		}
-		vehiclesCount.set(vehiclesMap.size());
-		vehiclesSpeed.set(((float) speedSum)/vehiclesMap.size());
-		scheduleCheckVehicleLocations();
-	}
+    private void checkVehicleLocations() {
+        Value[] vehicles = NextBusHttpAPI.getVehicleLocations(agency);
+        vehiclesMap.clear();
+        int speedSum = 0;
+        for (int i = 0; i < vehicles.length; i++) {
+            vehiclesMap.put(vehicles[i].get("id").stringValue(), vehicles[i]);
+            speedSum += Integer.parseInt(vehicles[i].get("speed").stringValue());
+        }
+        vehiclesCount.set(vehiclesMap.size());
+        vehiclesSpeed.set(((float) speedSum) / vehiclesMap.size());
+        scheduleCheckVehicleLocations();
+    }
 
-	private void scheduleCheckVehicleLocations() {
-		setTimer(10000, () -> checkVehicleLocations());
-	}
+    private void scheduleCheckVehicleLocations() {
+        setTimer(10000, () -> checkVehicleLocations());
+    }
 
-	@Override
-	public void didStart() {
-		vehiclesMap.clear();
-		System.out.println("Started Service" + nodeUri());
-		scheduleCheckVehicleLocations();
-		Value config = Record.of(new Slot("key", this.nodeUri().toUri()), new Slot("node", this.nodeUri().toUri()));
-		context.command("/transit/norcal", "agencies/add", config);
-	}
+    @Override
+    public void didStart() {
+        vehiclesMap.clear();
+        System.out.println("Started Service" + nodeUri());
+        scheduleCheckVehicleLocations();
+        Value config = Record.of(new Slot("key", this.nodeUri().toUri()), new Slot("node", this.nodeUri().toUri()));
+        context.command("/transit/norcal", "agencies/add", config);
+    }
 }
